1c8531f97d6f2488cc9186ea7c5bf507f7a239d1
BrandonMach <brandon.mach2003@gmail.com>
2022-07-05 20:49:06 +0200

    save

---
+++ 2 character puzzle 3D/Assets/MovmentScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    private float ySpeed;
+
+    float horizontalInput;
+    float verticalInput;
+
+    private CharacterController characterController;
+    public bool isActivePlayer;
+    private float originalStepOffset;
+
+    public CharacterController GetCharacterController
+    {
+        get { return characterController; }
+    }
+    public bool IsActivePlayer
+    {
+        get { return isActivePlayer; }
+        set { isActivePlayer = value; }
+    }
+
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset - 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            ySpeed = -0.5f;
+
+            characterController.stepOffset = originalStepOffset;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = jumpSpeed;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+
+
+
+        Vector3 velocity = movementDirection * magnitude; // velocity Y is 0 from movemnetDirecton
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    public void Movement()
+    {
+        if (isActivePlayer)//Cant move if object is not active player
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+        }
+
+
+    }
+}
